c7d3ab89972daad6865fda0d34553a5265efac1e
Marcin Tyszkiewicz <56219944+mtyszkiewicz@users.noreply.github.com>
2023-05-18 21:47:58 +0200

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,29 @@
 <script>
-  
-
-  
-  const API_BASE = "http://192.168.1.17:5055"
+  const API_BASE = "http://192.168.1.17:5055";
   export let data;
-  
+
+  const username = data.userSummary.user.name;
+  const image = data.userSummary.user.image;
+  $: score = data.userSummary.user.total_score;
+
+  function updateScore() {
+    score += 1;
+  }
+
   function refreshPage() {
-		location.reload();
-	};
+    location.reload();
+  }
 
   /**
    * @param {bigint} activityId
    */
   async function logActivity(activityId) {
-    const res = await fetch(
+    await fetch(
       `${API_BASE}/activities_log?user_id=${data?.user.id}&activity_id=${activityId}`,
-      { method: "POST"}
+      { method: "POST" }
     );
-    await res.json();
-    const res2 = await fetch(`${API_BASE}/summary`,);
-    data.summary = await res2.json();
     location.reload();
   }
-
-  async function updateSummary() {
-    
-  }
-
 </script>
 
 <main
@@ -36,21 +33,29 @@
     <div
       class="w-full max-w-xl p-12 mx-auto rounded-lg shadow-xl dark:bg-white/10 bg-white/30 ring-1 ring-gray-900/5 backdrop-blur-lg"
     >
-      <div class="flex justify-between items-center">
-        <p class="font-large">Aktywności</p>
-        <button
-          class="bg-gray-300 text-black px-2 py-1 rounded shadow hover:bg-gray-50 transition-colors duration-200"
-          on:click={refreshPage}>Refresh Page</button
-        >
+    <div class="bg-red-500 p-6">
+      <div class="flex items-center space-x-4">
+        <img
+          src={data?.user.image}
+          alt={data?.user.name}
+          width={64}
+          height={64}
+          class="rounded-full ring-1 ring-gray-900/5"
+        />
+        <div class="space-y-1">
+          <p class="font-medium leading-none">{data?.user.name}</p>
+          <p class="text-sm text-gray-500">Score: {score}</p>
+        </div>
       </div>
+    </div>
 
       <div class="divide-y divide-gray-900/5">
         {#each data?.activities as activity (activity.id)}
-          <div class="flex items-center justify-between py-3">
+          <div class="flex items-center justify-between py-2">
             <div class="flex items-center space-x-4">
               <div class="space-x-1">
-                <span class="font-medium leading-none">{activity.name}</span>
-                <span class="text-sm text-gray-500">{activity.score}</span>
+                <span class="font-sm leading-none">{activity.name}</span>
+                <span class="text-xs text-gray-500">{activity.score}</span>
                 <button
                   class="bg-gray-300 text-black px-2 py-1 rounded shadow hover:bg-gray-50 transition-colors duration-200"
                   on:click={() => logActivity(activity.id)}>Add</button
@@ -61,23 +66,23 @@
         {/each}
       </div>
 
-      <div class="divide-y divide-gray-900/5">
-        {#each data?.summary as user (user.user_id)}
+      <!-- <div class="divide-y divide-gray-900/5">
+        {#each data?.userSummary as summary (user.user_id)}
           <div class="flex items-center justify-between py-3">
             <div class="flex items-center space-x-4">
               <div class="space-x-1">
                 <span class="font-medium leading-none">{user.user_name}</span>
                 <span class="text-sm text-gray-500">{user.total_score}</span>
                 <ul>
-                {#each user.activities as activity}
-                  <li>{activity.activity_name} {activity.activity_count}</li>
-                {/each}
+                  {#each user.activities as activity}
+                    <li>{activity.activity_name} {activity.activity_count}</li>
+                  {/each}
                 </ul>
               </div>
             </div>
           </div>
         {/each}
-      </div>
+      </div> -->
     </div>
   {:else}
     <h1 class="text-red-600 text-5xl font-bold text-center">
