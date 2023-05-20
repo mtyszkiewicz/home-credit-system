933449e0c4054123a3f8cbe683ca6cbc0445d294
Marcin Tyszkiewicz <56219944+mtyszkiewicz@users.noreply.github.com>
2023-05-20 17:34:13 +0200

    migrate from neon postgres to local container

--- backend/hsc_api/database.py
+++ backend/hsc_api/database.py
@@ -4,12 +4,17 @@ from sqlalchemy.orm import sessionmaker
 
 from sqlalchemy import create_engine
 
-USERNAME = "default"
-PASSWORD = "zOLWlj2ky3rf"
-HOST = "ep-twilight-wildflower-510519.eu-central-1.postgres.vercel-storage.com"
-CONNSTR = f'postgresql://{USERNAME}:{PASSWORD}@{HOST}/verceldb'
+# POSTGRES_USER = "default"
+# POSTGRES_PASSWORD = "zOLWlj2ky3rf"
+# POSTGRES_HOST = "ep-twilight-wildflower-510519.eu-central-1.postgres.vercel-storage.com"
+# POSTGRES_DB ="verceldb"
+POSTGRES_USER = "admin"
+POSTGRES_PASSWORD = "5o3B1Lh6kJsH5ZEACndao3eG"
+POSTGRES_HOST = "localhost:5432"
+POSTGRES_DB ="home"
+CONNSTR = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}'
 
-engine = create_engine(CONNSTR, connect_args={'sslmode':'require', 'options=project': 'ep-twilight-wildflower-510519'})
+engine = create_engine(CONNSTR)#, connect_args={'sslmode':'require', 'options=project': 'ep-twilight-wildflower-510519'})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- database/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-connection_string='postgres://default:zOLWlj2ky3rf@ep-twilight-wildflower-510519.eu-central-1.postgres.vercel-storage.com:5432/verceldb?options=project%3Dep-twilight-wildflower-510519-pooler'
-
-psql "$connection_string" -f ./entrypoint.sql
