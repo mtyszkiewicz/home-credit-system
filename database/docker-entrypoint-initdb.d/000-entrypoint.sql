895c9da0d6000289e27a9eb69a456f829283152f
Marcin Tyszkiewicz <56219944+mtyszkiewicz@users.noreply.github.com>
2023-05-23 17:37:07 +0200

    add Zakupy to activities

--- database/docker-entrypoint-initdb.d/000-entrypoint.sql
+++ database/docker-entrypoint-initdb.d/000-entrypoint.sql
@@ -16,17 +16,14 @@ INSERT INTO activities (name, icon, value, group_name, cooldown_minutes) VALUES
 ('Wyrzucenie śmieci', '🗑️', 2, 'standard', 1),
 ('Wstawienie pralki', '🧺', 2, 'standard', 60),
 ('Powieszenie prania', '☀️', 2, 'standard', 60),
-('Ubrania do szafy', '👔', 3, 'standard', 60);
-
-INSERT INTO activities (name, icon, value, group_name, cooldown_minutes) VALUES
+('Ubrania do szafy', '👔', 3, 'standard', 60),
 ('Umycie zlewu', '🚰', 3, 'extra', 120),
-('Odkurzenie domu', '🌪️', 5, 'extra', 1440),                     -- 1 day
-('Umycie umywalki', '🧼', 3, 'extra', 1440),            -- 1 day
-('Usunięcie owłosienia', '💩', 2, 'extra', 20160),       -- 2 weeks
-('Umycie kabiny prysznicowej', '🚿', 5, 'extra', 1440); -- 1 day
-
-INSERT INTO activities (name, icon, value, group_name, cooldown_minutes) VALUES
-('Ubrania na ziemi', '🧦', -3, 'penalty', 1);
+('Odkurzenie domu', '🌪️', 5, 'extra', 1440),
+('Umycie umywalki', '🧼', 3, 'extra', 1440),
+('Usunięcie owłosienia', '💩', 2, 'extra', 20160),
+('Umycie kabiny prysznicowej', '🚿', 5, 'extra', 1),
+('Ubrania na ziemi', '🧦', -3, 'penalty', 1),
+('Zakupy', '🥐', 3, 'standard', 60);
 
 CREATE TABLE IF NOT EXISTS activity_records (
     id SERIAL PRIMARY KEY,
