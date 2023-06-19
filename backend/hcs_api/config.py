698bb75a37d2a21c3e84aea8b2c89db4bd88dbcc
Marcin Tyszkiewicz <56219944+mtyszkiewicz@users.noreply.github.com>
2023-06-19 17:09:52 +0200

    add backend to docker compose

--- backend/hcs_api/config.py
+++ backend/hcs_api/config.py
@@ -4,7 +4,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     postgres_user: str
     postgres_password: str
-    postgres_host: str = "localhost:5432"
+    postgres_host: str = "database:5432"
     postgres_db: str = "home"
 
 
