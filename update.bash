13e88eccffd96488d7d408c046a7510ddd53059b
Marcin Tyszkiewicz <56219944+mtyszkiewicz@users.noreply.github.com>
2023-06-19 18:10:17 +0200

    update scripts for backup and update

--- update.bash
+++ update.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source .env;
-psql "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOSTNAME:5432/$POSTGRES_DB" \
+psql "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" \
  -f './database/docker-entrypoint-initdb.d/000-entrypoint.sql';
