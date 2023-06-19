a3705b12fbb5ecee986bc74ce36769d5327f8744
Marcin Tyszkiewicz <56219944+mtyszkiewicz@users.noreply.github.com>
2023-06-19 18:09:36 +0200

    create separate postgres port env variable

--- backend/hcs_api/config.py
+++ backend/hcs_api/config.py
@@ -4,8 +4,13 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     postgres_user: str
     postgres_password: str
-    postgres_host: str = "database:5432"
-    postgres_db: str = "home"
+    postgres_port: str
+    postgres_db: str
+
+    @property
+    def postgres_host(self):
+        """Postgres host in docker network is always 'database'"""
+        return "database"
 
 
 settings = Settings()
--- backend/hcs_api/database.py
+++ backend/hcs_api/database.py
@@ -4,7 +4,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(
-    f"postgresql://{settings.postgres_user}:{settings.postgres_password}@{settings.postgres_host}/{settings.postgres_db}"
+    "postgresql://{0}:{1}@{2}:{3}/{4}".format(
+        settings.postgres_user,
+        settings.postgres_password,
+        settings.postgres_host,
+        settings.postgres_port,
+        settings.postgres_db,
+    )
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
