1f1ccf94dc9218d66f60873a112f71507bacc122
Marcin Tyszkiewicz <56219944+mtyszkiewicz@users.noreply.github.com>
2023-07-14 15:17:51 +0200

    remove docker compose prod approach

--- reload-db.sh
+++ reload-db.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+source .env
 psql "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" \
  -f './database/docker-entrypoint-initdb.d/000-entrypoint.sql';
